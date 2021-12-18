24b192ceab84f9c891e34d320b222f94dc69c96f
Ubaid Siddiqui <ubdsiddiqui404@gmail.com>
2021-12-18 09:31:19 +0530

    new repo and files added for core java practice.

---
+++ ComputeCircleArea.java
@@ -0,0 +1,28 @@
+package corejavapractice;
+
+import java.util.Scanner;
+
+
+public class ComputeCircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter radius: ");
+		double radius = input.nextDouble();
+		
+		double area = computeAreaOfCircle(radius);
+		System.out.println("area is: " + area);
+
+	}
+	
+	public static double computeAreaOfCircle(double radius) {
+		
+		double area = radius * radius * 3.14159;
+		
+		return area;
+	}
+
+}
---
+++ package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author NC
+ *
+ */
+package corejavapractice;
