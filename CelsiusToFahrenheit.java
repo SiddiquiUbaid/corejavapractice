b1d8fddaa3c00c1489f48384ea76f3fa20d892a9
Ubaid Siddiqui <ubdsiddiqui404@gmail.com>
2021-12-20 12:54:00 +0530

    added stepwise comments.

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -7,13 +7,17 @@ public class CelsiusToFahrenheit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// create Scanner object.
 		Scanner input = new Scanner(System.in);
 		
+		// take input from user.
 		System.out.println("Enter value in Celsius: ");
 		double celsius = input.nextDouble();
 		
+		// convert celsius to fahrenhiet.
 		double fahrenheit = (9.0 / 5) * celsius + 32;
 		
+		// print the result.
 		System.out.println( celsius + " is " + fahrenheit + " fahrenheit");
 
 	}
